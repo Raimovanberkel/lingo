f0601c4913c0da642e804587a115e4d5a3901f8a
MSI\Raimo van Berkel <Raimo.van.berkel@gmail.com>
2017-08-22 16:38:48 +0200

    no message

--- lingo.js
+++ lingo.js
@@ -6,10 +6,9 @@ var words = [];
 var correctLetters = [currentWord[0],"","","",""];
 var attempts = 0;
 
-  function getRandomWord(){
+function getRandomWord(){
     return words[Math.floor(Math.random() * (words.length -1))];
-}
-  console.log(currentWord);
+}   console.log(currentWord);
 
 function getInput(){
     var chars = inputThing.value.split("").slice(0, 5);
@@ -30,6 +29,16 @@ function compareWords(){
     }
 }
 
+function letterIsInWord(letter, word){
+    for(var i = 0; i < 5; i++ ){
+
+        if(letter === word[i]){
+            return true;
+        }
+    }
+    return false;
+}
+
 window.addEventListener("keydown", function(e){
     if (e.key ==="Enter"){
         input = document.getElementById("input").value;
@@ -39,9 +48,16 @@ window.addEventListener("keydown", function(e){
         for(var i = 0; i < 5; i++){
             if(words[attempts][i] === correctLetters[i] && words[attempts][i] !== ""){
                 rows[attempts].innerHTML +=
-                "<div style='background: green'>" +
+                "<div style='background: red'>" +
                   words[attempts][i] +
                  "</div>";
+            }
+
+            else if(letterIsInWord(words[attempts][i], currentWord)){
+                rows[attempts].innerHTML +=
+                    "<div style='background: yellow'>" +
+                    words[attempts][i] + 
+                    "</div>";   
             }
          
             else {
@@ -56,13 +72,12 @@ function reset() {
   console.log(input + " < input, currentWord > " + currentWord);
 if (input == currentWord) {
   alert("Je hebt het woord geraden!");
-  location.reload();
 }
 }
 
 
 for(var i = 0; i < 5; i++){
-    rows[i].innerHTML += "<div style='background: green'>" + currentWord[0] + "</div>";
+    rows[i].innerHTML += "<div style='background: red'>" + currentWord[0] + "</div>";
     for(var j = 0; j < 4; j++){
         rows[i].innerHTML += "<div></div>";
     }
